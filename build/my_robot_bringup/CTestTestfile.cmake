03a245ac98aa1b161488724597cf0a54c3cb88aa
rick <rig12332113@gmail.com>
2025-06-28 06:10:35 -0700

    combine arm with cart

--- build/my_robot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rick/ros2_ws/src/my_robot_bringup
-# Build directory: /home/rick/ros2_ws/src/build/my_robot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- install/my_robot_bringup/share/my_robot_bringup/cmake/my_robot_bringupConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/rick/ros2_ws/src/build/my_robot_bringup/ament_cmake_core/my_robot_bringupConfig.cmake
